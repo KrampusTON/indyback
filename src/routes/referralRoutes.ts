5e99b06a5c9146218a4d04984614b851ce432732
KrampusTON <jalomelo111@gmail.com>
2025-05-07 11:40:12 +0200

    Update referralRoutes.ts

--- src/routes/referralRoutes.ts
+++ src/routes/referralRoutes.ts
@@ -5,7 +5,6 @@ import { ReferralController } from '../controllers/referralController';
 const router = Router();
 const controller = new ReferralController();
 
-// Routy s asyncHandler pre spracovanie chýb
 router.post('/register', asyncHandler(controller.registerUser.bind(controller)));
 router.get('/stats/:address', asyncHandler(controller.getReferralStats.bind(controller)));
 router.get('/tree/:address', asyncHandler(controller.getReferralTree.bind(controller)));
